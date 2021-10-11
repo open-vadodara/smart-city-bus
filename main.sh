f7c2ca0a087397f4b22c3dc8a00b74a05d40faa3
kaizer1v <kaizer1v@gmail.com>
2021-10-11 17:09:05 +0530

    timetable data for each bus

--- main.sh
+++ main.sh
@@ -3,11 +3,13 @@
 date=$(date '+%d-%b-%Y')
 IFS='\n'
 temp='temp'
+tt='temp_tt'
 json='route_details'
 mkdir $temp
+mkdir $tt
 mkdir $json
 
-echo "# Downloading from website..."
+echo "# Downloading route timings from website..."
 while IFS= read -r line
 do
   id=$(echo $line | cut -d "-" -f 1)
@@ -15,9 +17,21 @@ do
   curl "14.98.182.250:4016/Route/GetRouteTripTimingDetails?Date=$date&RouteID=$id" --output "$temp/$route_no.txt"
 done < bus_numbers.txt
 
-echo "# Processing raw data and converting into json"
+echo "# Processing raw data and converting into json..."
 node process.js $temp $json
 
-echo "# Done!"
+echo "# Downloading timetables from website..."
+while IFS= read -r line
+do
+  route_no=$(echo $line | cut -d "-" -f 1)
+  timetable_id=$(echo $line | cut -d "-" -f 2)
+  curl "http://14.98.182.250:4016/Route/GetRouteTripDetails?TimeTableTripID=$timetable_id&ScheduleDate=$date" --output "$temp/tt_$route_no.txt"
+done < timetables.txt
 
-rm -rf $temp
+echo "# Processing raw timetable data and converting into json..."
+node process.js $tt $json true
+
+echo "# Done..."
+
+# rm -rf $temp
+# rm -rf $tt
--- process.js
+++ process.js
@@ -4,6 +4,7 @@ const path = require('path')
 // Input & Output directory paths as parameters
 let input_dir = process.argv[2],
     output_dir = process.argv[3]
+    is_tt = process.argv[4] ? true : false
 
 // Read every file as raw input, convert into JSON & save in output dir
 fs.readdir(input_dir, (err, filenames) => {
@@ -11,15 +12,33 @@ fs.readdir(input_dir, (err, filenames) => {
     console.error(err)
     return
   }
+  let timetables = []
 
   filenames.forEach((filename) => {
     let deets = fs.readFileSync(path.join(__dirname, input_dir, filename)).toString()
+
     let route_details = JSON.parse(deets),
-        fname = filename.split('.')[0],
+        fname = filename.split('.')[0],  // remove `.txt` from end
         save_as = path.join(output_dir, `${fname}.json`)
 
+    let bus_obj = JSON.parse(route_details)
+
+    if(!is_tt) {
+      // save array's first item's timetable id
+      timetables.push(`${fname}-${bus_obj[0]['TIME_TABLE_TRIP_ID']}`)
+    }
+
     fs.writeFile(save_as, route_details, 'utf8', () => {
-      console.log(`${fname}.json file saved`)
+      console.log(`${fname}.json file saved...`)
     })
   })
+
+  if(!is_tt) {
+    // Save timetables for all buses into separate txt file to parse
+    fs.writeFile(path.join(__dirname, 'timetables.txt'), timetables.join('\n'), 'utf8', () => {
+      console.log('timetables.txt file saved...')
+    })
+  }
 })
+
+
